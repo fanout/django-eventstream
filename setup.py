34b4e21593497da6c7ad4c4274a1e1b5d3857f10
Justin Karneges <justin@karneges.com>
2017-07-13 16:09:50 -0700

    fix packaging

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
-#!/usr/bin/env python
-
+import os
 from setuptools import setup
 
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+	readme = f.read()
+
 setup(
-name="django-eventstream",
-version="0.1.0",
-description="Django EventStream library",
-author="Justin Karneges",
-author_email="justin@karneges.com",
-url="https://github.com/fanout/django-eventstream",
-license="MIT",
-package_dir={'django_eventstream': 'src'},
-packages=['django_eventstream'],
-install_requires=["django_grip>=1.7,<3", "Werkzeug>=0.12,<1"],
+name='django-eventstream',
+version='0.2.0',
+description='Django EventStream library',
+long_description=readme,
+author='Justin Karneges',
+author_email='justin@karneges.com',
+url='https://github.com/fanout/django-eventstream',
+license='MIT',
+zip_safe=False,
+packages=['django_eventstream', 'django_eventstream.migrations'],
+package_data={'django_eventstream': ['static/django_eventstream/*']},
+install_requires=['django_grip>=1.7,<3', 'Werkzeug>=0.12,<1'],
 classifiers=[
-        "Topic :: Utilities",
-        "License :: OSI Approved :: MIT License"
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License'
 ]
 )
