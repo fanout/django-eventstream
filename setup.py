2b42d9de551caa6c96ca892a5d901d89b8bec181
Justin Karneges <justin@karneges.com>
2017-08-17 20:00:12 -0700

    update meta info

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
 
 setup(
 name='django-eventstream',
-version='1.1.1',
+version='1.1.2',
 description='Django EventStream library',
 long_description=readme,
 author='Justin Karneges',
@@ -18,7 +18,12 @@ packages=['django_eventstream', 'django_eventstream.migrations', 'django_eventst
 package_data={'django_eventstream': ['static/django_eventstream/*']},
 install_requires=['django_grip>=1.7,<3', 'Werkzeug>=0.12,<1', 'six>=1.10,<2'],
 classifiers=[
-        'Topic :: Utilities',
-        'License :: OSI Approved :: MIT License'
+	'Development Status :: 4 - Beta',
+	'Topic :: Utilities',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python',
+	'Programming Language :: Python :: 2',
+	'Programming Language :: Python :: 3',
+	'Framework :: Django',
 ]
 )
