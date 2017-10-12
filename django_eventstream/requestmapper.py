a3d07fe9b3ae64a049c85daebf51ecdac474839e
Justin Karneges <justin@karneges.com>
2017-10-11 20:22:22 -0700

    support channel keyword

--- django_eventstream/requestmapper.py
+++ django_eventstream/requestmapper.py
@@ -5,12 +5,14 @@ class RequestMapperBase(object):
 class DefaultRequestMapper(RequestMapperBase):
 	def get_channels_for_request(self, request, view_kwargs):
 		# by default, use view keywords, else query params
-		if 'channels' in view_kwargs:
-			return set(view_kwargs['channels'])
-		elif 'format-channels' in view_kwargs:
+		if 'format-channels' in view_kwargs:
 			out = set()
 			for format_channel in view_kwargs['format-channels']:
 				out.add(format_channel.format(**view_kwargs))
 			return out
+		elif 'channels' in view_kwargs:
+			return set(view_kwargs['channels'])
+		elif 'channel' in view_kwargs:
+			return set([view_kwargs['channel']])
 		else:
 			return set(request.GET.getlist('channel'))
