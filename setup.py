58d3317874241c69f765e165400981aecd17b46a
Justin Karneges <justin@karneges.com>
2017-11-08 12:09:10 -0800

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
 
 setup(
 name='django-eventstream',
-version='1.2.0',
+version='2.0.0',
 description='Django EventStream library',
 long_description=readme,
 author='Justin Karneges',
