20d31948f62d0485a106544eba35014eb3f7cea9
Justin Karneges <justin@karneges.com>
2019-03-02 22:01:40 -0800

    chat2 nits

--- examples/chat2/server/settings.py
+++ examples/chat2/server/settings.py
@@ -83,12 +83,10 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(default='sqlite:///db.sqlite3')
 
 
 # Password validation
