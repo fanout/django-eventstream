37f3cda6574721f98831a71863332d06074ed5cd
Justin Karneges <justin@karneges.com>
2019-03-06 18:26:30 -0800

    chat: set STATIC_ROOT

--- examples/chat/server/settings.py
+++ examples/chat/server/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 ASGI_APPLICATION = 'server.routing.application'
 
