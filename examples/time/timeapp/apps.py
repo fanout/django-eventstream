b4ce4d4dfb5298aa8b60e8dc4770dc9133125f61
Justin Karneges <justin@karneges.com>
2020-06-18 09:51:34 -0700

    time example: start worker thread using AppConfig.ready()

--- examples/time/timeapp/apps.py
+++ examples/time/timeapp/apps.py
@@ -1,5 +1,46 @@
+import time
+import datetime
+import threading
 from django.apps import AppConfig
+from django_eventstream import send_event
 
 
 class TimeappConfig(AppConfig):
-    name = 'chat'
+    name = 'timeapp'
+
+    def ready(self):
+        ensure_worker_started()
+
+
+worker_started = False
+
+def ensure_worker_started():
+    global worker_started
+
+    if worker_started:
+        return
+
+    if not is_db_ready():
+        return
+
+    worker_started = True
+
+    thread = threading.Thread(target=send_worker)
+    thread.daemon = True
+    thread.start()
+
+def send_worker():
+    while True:
+        data = datetime.datetime.utcnow().isoformat()
+        send_event('time', 'message', data)
+        time.sleep(1)
+
+def is_db_ready():
+    from django.db import DatabaseError
+    from django_eventstream.models import Event
+    try:
+        # see if db tables are present
+        Event.objects.count()
+        return True
+    except DatabaseError:
+        return False
--- examples/time/timeapp/views.py
+++ examples/time/timeapp/views.py
@@ -1,33 +1,8 @@
-import time
-import threading
-import datetime
 from django.shortcuts import render
-from django_eventstream import get_current_event_id, send_event
-from django_eventstream.channelmanager import DefaultChannelManager
+from django_eventstream import get_current_event_id
 
 def home(request):
 	context = {}
 	context['url'] = '/events/'
 	context['last_id'] = get_current_event_id(['time'])
 	return render(request, 'timeapp/home.html', context)
-
-def _send_worker():
-	while True:
-		data = datetime.datetime.utcnow().isoformat()
-		send_event('time', 'message', data)
-		time.sleep(1)
-
-def _db_ready():
-	from django.db import DatabaseError
-	from django_eventstream.models import Event
-	try:
-		# see if db tables are present
-		Event.objects.count()
-		return True
-	except DatabaseError:
-		return False
-
-if _db_ready():
-	send_thread = threading.Thread(target=_send_worker)
-	send_thread.daemon = True
-	send_thread.start()
