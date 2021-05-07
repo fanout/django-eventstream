479114acb8f0a57a3354ca724be102d7661c5621
Justin Karneges <justin@karneges.com>
2021-05-07 10:00:02 -0700

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires.extend(['PyJWT>=1.5,<3', 'gripcontrol>=4.0,<5', 'django_grip>=3
 
 setup(
 name='django-eventstream',
-version='4.1.0',
+version='4.2.0',
 description='Server-Sent Events for Django',
 long_description=readme,
 long_description_content_type='text/markdown',
