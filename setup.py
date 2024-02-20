c1bba1c06f0d3cd5ed8e0546dfc0a3c4d05d00b6
Justin Karneges <jkarneges@fastly.com>
2024-02-20 14:42:15 -0800

    update version

--- setup.py
+++ setup.py
@@ -4,15 +4,9 @@ from setuptools import setup
 with open(os.path.join(os.path.dirname(__file__), "README.md")) as f:
     readme = f.read()
 
-install_requires = []
-
-install_requires.extend(
-    ["PyJWT>=1.5,<3", "gripcontrol>=4.0,<5", "django_grip>=3.0,<4", "six>=1.10,<2"]
-)
-
 setup(
     name="django-eventstream",
-    version="4.5.1",
+    version="5.0.0",
     description="Server-Sent Events for Django",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -28,7 +22,13 @@ setup(
         "django_eventstream.management.commands",
     ],
     package_data={"django_eventstream": ["static/django_eventstream/*"]},
-    install_requires=install_requires,
+    install_requires=[
+        "Django>=5.0",
+        "PyJWT>=1.5,<3",
+        "gripcontrol>=4.0,<5",
+        "django_grip>=3.0,<4",
+        "six>=1.10,<2",
+    ],
     tests_require=["Django>=2.0"],
     test_suite="tests.runtests.runtests",
     classifiers=[
