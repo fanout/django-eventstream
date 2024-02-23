d5809c11c942d456d80f0b927dbcca065335e284
Justin Karneges <jkarneges@fastly.com>
2024-02-23 10:30:28 -0800

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as f:
 
 setup(
     name="django-eventstream",
-    version="5.0.0",
+    version="5.0.1",
     description="Server-Sent Events for Django",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -23,7 +23,7 @@ setup(
     ],
     package_data={"django_eventstream": ["static/django_eventstream/*"]},
     install_requires=[
-        "Django>=5.0",
+        "Django>=5",
         "PyJWT>=1.5,<3",
         "gripcontrol>=4.0,<5",
         "django_grip>=3.0,<4",
