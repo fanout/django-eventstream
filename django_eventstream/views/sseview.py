9f5b978c6c69a4d402164a9f6f5adf844c6e2a8a
Fournet Enzo <enzofournet31@gmail.com>
2024-05-02 16:53:03 +0200

    Adding support for disable browsable view
    Adding everything needed to be able to disable browsable view.

--- django_eventstream/views/sseview.py
+++ django_eventstream/views/sseview.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
+from rest_framework.settings import APISettings
 from django_eventstream.views.eventstreamerview import events
 from django_eventstream.renderers import SSEEventRenderer, BrowsableAPIEventStreamRenderer
 
@@ -22,11 +24,37 @@ class EventsViewSet(ViewSet):
     
     http_method_names = ['get']
     renderer_classes = (BrowsableAPIEventStreamRenderer, SSEEventRenderer)
+    
+    no_api_sse_renderer = False
 
     def __init__(self, channels: list = None, messages_types: list = None, *args, **kwargs):
         super().__init__()
         self.channels = channels if channels is not None else []
         self.messages_types = messages_types if messages_types is not None else None
+        
+    def get_renderers(self):
+        if hasattr(settings, 'REST_FRAMEWORK'):
+            api_settings = APISettings(user_settings=settings.REST_FRAMEWORK, defaults=None, import_strings=None)
+            default_renderers = list(api_settings.DEFAULT_RENDERER_CLASSES)
+            
+            sse_renderers = []
+            api_sse_renderers = []
+            
+            for renderer_class in default_renderers:
+                renderer_instance = renderer_class()
+                if renderer_instance.format == 'api_sse' :
+                    api_sse_renderers.append(renderer_class)
+                if renderer_instance.format == 'text/event-stream':
+                    sse_renderers.append(renderer_class)
+
+            if len(api_sse_renderers) == 0:
+                self.no_api_sse_renderer = True
+            else :
+                self.no_api_sse_renderer = False
+            
+            self.renderer_classes = api_sse_renderers + sse_renderers
+            
+        return super().get_renderers()
     
     def list(self, request):
         if len(self.channels) > 0 and request.query_params.get('channels'):
@@ -49,19 +77,20 @@ class EventsViewSet(ViewSet):
             return Response({"error": "Conflicting channel specifications in URL and ViewSet configuration."}, status=status.HTTP_400_BAD_REQUEST)
         return self._stream_or_respond([channel], request._request)
     
-    def _stream_or_respond(self, channels, django_request):
-        django_request = django_request._request
-        kwargs = {'channels': channels}
-        if 'text/event-stream' in django_request.META.get('HTTP_ACCEPT', '') or 'text/event-stream' in django_request.GET.get('format', ''):
-            return events(django_request, **kwargs)
-        elif 'text/html' in django_request.META.get('HTTP_ACCEPT', '') or 'text/html' in django_request.GET.get('format', ''):
-            data = {'channels': ', '.join(channels)} if channels else {}
-            data['messages_types'] = ', '.join(self.messages_types) if self.messages_types else "message"
-            print("data['messages_types']")
-            print(data['messages_types'])
+    def _accepted_format(self, request, format_list):
+        accept_header = request.META.get('HTTP_ACCEPT', '')
+        query_format = request.GET.get('format', '')
+        return any(fmt in accept_header or fmt in query_format for fmt in format_list)
 
+    def _stream_or_respond(self, channels, django_request):
+        request = django_request._request
+        data = {'channels': ', '.join(channels) or "", 'messages_types': ', '.join(self.messages_types) or "message"}
+
+        if self._accepted_format(request, ['text/html']):
             return Response(data, status=status.HTTP_200_OK)
-        
+        elif self._accepted_format(request, ['text/event-stream', '*/*']):
+            return events(request, {'channels': channels})
+
         return Response({"error": "This endpoint only supports text/event-stream and text/html accept types."}, status=status.HTTP_400_BAD_REQUEST)
 
 def configure_events_view_set(channels=None, messages_types=None):
